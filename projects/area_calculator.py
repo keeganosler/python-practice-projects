a80b9c0a1413501c83be1c87406d4ccd1fe8a2d9
keeganosler <oslerkeegan@gmail.com>
2019-02-12 12:11:53 -0700

    add area calculator

---
+++ projects/area_calculator.py
@@ -0,0 +1,17 @@
+# area calculator computes and prints the area of shapes
+
+option = input("Enter C for circle or T for triangle: ")
+
+if option == "C":
+    shape = "circle"
+    radius = float(input("Please enter the radius of your circle: "))
+    area = 3.14159*(radius**2)
+elif option == "T":
+    shape = "triangle"
+    base = float(input("Please enter the base of your triangle: "))
+    height = float(input("Please enter the height of your triangle: "))
+    area = 0.5*base*height
+else:
+    print("Invalid shape selected!")
+
+print("Your shape is a " + shape + " and it's area is %s" %(area))
