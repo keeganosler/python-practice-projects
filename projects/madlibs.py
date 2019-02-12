fbaeb8328e9ace5dd4c182af603481cc7f5353df
keeganosler <oslerkeegan@gmail.com>
2019-02-12 12:01:53 -0700

    madlibs

---
+++ projects/madlibs.py
@@ -0,0 +1,27 @@
+#!/bin/python3
+
+#This program generates passages that are generated in mad-lib format
+
+
+# The template for the story
+
+STORY = "This morning _ woke up feeling _. 'It is going to be a _ day!' Outside, a bunch of _s were protesting to keep _ in stores. They began to _ to the rhythm of the _, which made all the _s very _. Concerned, _ texted _, who flew _ to _ and dropped _ in a puddle of frozen _. _ woke up in the year _, in a world where _s ruled the world."
+
+print('mad libs has started!')
+
+name = input("Enter a name: ")
+adjective1 = input("Enter an adjective: ")
+adjective2 = input("Enter an adjective: ")
+adjective3 = input("Enter an adjective: ")
+verb = input("Enter a verb: ")
+noun1 = input("Enter a noun: ")
+noun2 = input("Enter a noun: ")
+animal = input("Enter an animal: ")
+food = input("Enter a food: ")
+fruit = input("Enter a fruit: ")
+superhero = input("Enter a superhero: ")
+country = input("Enter a country: ")
+dessert = input("Enter a dessert: ")
+year = input("Enter a year: ")
+
+print("This morning" " woke up feeling _. 'It is going to be a _ day!' Outside, a bunch of _s were protesting to keep _ in stores. They began to _ to the rhythm of the _, which made all the _s very _. Concerned, _ texted _, who flew _ to _ and dropped _ in a puddle of frozen _. _ woke up in the year _, in a world where _s ruled the world.")
